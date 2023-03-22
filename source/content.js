73f3a4060104226f13e3135fab4f54211279da45
Milan G <dev@milangress.de>
2023-03-23 00:00:02 +0100

    firs working prototype

--- source/content.js
+++ source/content.js
@@ -1,6 +1,72 @@
 import optionsStorage from './options-storage.js';
 
 console.log('💈 Content script loaded for', chrome.runtime.getManifest().name);
+
+// document.body.style.backgroundColor = "orange";
+
+// console.log('doc', document.getElementById('editorcontainer'))
+// console.log('iframe', document.querySelector('iframe'))
+
+
+// const test = document.querySelector('div')
+// console.log('test', test)
+
+
+chrome.storage.sync.get("doSpellcheck").then((result) => {
+	console.log("Value currently is ", result.doSpellcheck);
+	if (result.doSpellcheck) {
+		setTimeout(spellcheck, 3000)
+	}
+});
+
+function spellcheck(enable = true) {
+	const outerFrame = document.querySelector('#editorcontainer').firstChild
+	console.log('outerFrame', outerFrame)
+	const innerFrame = outerFrame.contentWindow.document.querySelector('iframe')
+	console.log('innerFrame', innerFrame)
+
+	const innerDoc = innerFrame.contentWindow.document.querySelector('#innerdocbody')
+	console.log('innerDoc', innerDoc)
+
+
+	const spellcheck = {
+		enable: () => {
+			console.log('enable spellcheck')
+			innerDoc.setAttribute('spellcheck', 'true')
+		},
+		disable: () => {
+			console.log('disable spellcheck')
+			innerDoc.setAttribute('spellcheck', 'false')
+		},
+	}
+	if(enable) {
+		spellcheck.enable()
+	} else {
+		spellcheck.disable()
+	}
+}
+
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+	if (data) {
+		// Do something amazing
+	}
+	sendResponse({
+		received: true
+	});
+});
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+// 	console.log('message', request, sender)
+// 	if (request === 'spellcheck_enable') {
+// 		spellcheck(true)
+// 	}
+// 	else if (request === 'spellcheck_disable') {
+// 		spellcheck(false)
+// 	}
+// });
+
+
+/*
 async function init() {
 	const options = await optionsStorage.getAll();
 	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
@@ -11,6 +77,36 @@ async function init() {
 	notice.id = 'text-notice';
 	notice.style.border = '2px solid ' + color;
 	notice.style.color = color;
-}
 
-init();
+
+	function setUp() {
+		const test = document.querySelector('div')
+		console.log('test', test)
+
+		const outerFrame = document.querySelector('iframe[name="ace_outer"]')
+		console.log('outerFrame', outerFrame)
+		const innerFrame = outerFrame.contentWindow.document.querySelector('iframe')
+		console.log('innerFrame', innerFrame)
+		const innerDoc = document.querySelector('#innerdocbody')
+
+		console.log('innerDoc', innerDoc)
+
+		const spellcheck = {
+			enable: () => {
+				console.log('enable spellcheck')
+				innerDoc.setAttribute('spellcheck', 'true')
+			},
+			disable: () => {
+				console.log('disable spellcheck')
+				innerDoc.setAttribute('spellcheck', 'false')
+			},
+		}
+		spellcheck.enable()
+	}
+
+	setUp()
+
+}
+*/
+
+// init();
